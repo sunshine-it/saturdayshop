068d1b694d2ad727eccbbd15fd59cfccd119412e
vagrant <vagrant@homestead>
2019-08-19 09:52:38 +0000

    管理后台 - 新增、编辑商品

--- config/filesystems.php
+++ config/filesystems.php
@@ -66,9 +66,9 @@ return [
 
         'admin' => [
             'driver'     => 'local',
-            'root'       => public_path('upload'),
+            'root'       => storage_path('app/public'),
+            'url' => env('APP_URL').'/storage',
             'visibility' => 'public',
-            'url' => env('APP_URL').'/public/upload/',
         ],
 
     ],
