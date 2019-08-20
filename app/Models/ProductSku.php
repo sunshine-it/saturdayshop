d2985b49f5f61636aa4614a424d37aa32e40ec8e
vagrant <vagrant@homestead>
2019-08-20 12:27:44 +0000

    创建订单

--- app/Models/ProductSku.php
+++ app/Models/ProductSku.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Exceptions\InternalException;
 
 // 产品的 SKU 表模型类
 class ProductSku extends Model
@@ -13,5 +14,23 @@ class ProductSku extends Model
     public function product()
     {
         return $this->belongsTo(Product::class);
+    }
+
+    // 减库存
+    public function decreaseStock($amount)
+    {
+        if ($amount < 0) {
+            throw new InternalException('减库存不可小于0');
+        }
+        return $this->where('id', $this->id)->where('stock', '>=', $amount)->decrement('stock', $amount);
+    }
+
+    // 加库存
+    public function addStock($amount)
+    {
+        if ($amount < 0) {
+            throw new InternalException('加库存不可小于0');
+        }
+        $this->increment('stock', $amount);
     }
 }
