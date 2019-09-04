bfb96d547b73e38f9d3985497552b40d5fcce3b2
Allen <pazmnew@163.com>
2019-09-04 11:38:10 +0000

    fix

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,1 @@
 <?php
-
-// 为了解决不支持对 api/user 这个闭包路由做缓存
-// use Illuminate\Http\Request;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-// Route::middleware('auth:api')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
